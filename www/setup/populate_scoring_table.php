07e3ad13333232d5c80bbb5b16e00063f52a1ed3
Rupert Chen <rupert.chen@gmail.com>
2007-03-08 09:57:46 +0000

    Adding in ability to populate group event scoring entries.

--- www/setup/populate_scoring_table.php
+++ www/setup/populate_scoring_table.php
@@ -10,30 +10,58 @@
     // Create stuff
     if ($doCreate) {
         // Queries
-        // Get form blow out that fits each registration entry
-	$q0 = 'SELECT r.registration_id, r.competitor_id, c.cmat_year, fb.form_blowout_id, c.level_id, c.gender_id, c.age_group_id, fb.age_group_id, r.form_id'
-	    . ' FROM cmat_annual.registration r, cmat_annual.competitor c, cmat_annual.form_blowout fb'
+        // Get form blowout that fits each individual event registration entry
+	$q0 = 'SELECT r.registration_id, r.competitor_id, c.cmat_year, fb.form_blowout_id'
+	    . ' FROM cmat_annual.registration r, cmat_annual.competitor c, cmat_annual.form_blowout fb, cmat_enum.form f'
 	    . ' WHERE r.scoring_id is null'
+            . ' AND r.is_paid = true'
             . ' AND c.cmat_year = 15'
+            . ' AND f.is_group = false'
             . ' AND r.competitor_id = c.competitor_id'
+            . ' AND r.form_id = f.form_id'
             . ' AND fb.cmat_year = c.cmat_year'
             . ' AND fb.form_id = r.form_id'
             . ' AND fb.gender_id = c.gender_id'
             . ' AND fb.level_id = c.level_id'
             . ' AND (fb.age_group_id = c.age_group_id OR (c.age_group_id = 8 AND fb.age_group_id in (4, 5)) OR (c.age_group_id = 9 AND fb.age_group_id in (5, 6)) OR (c.age_group_id = 10 AND fb.age_group_id in (6, 7)))';
-        // Create a new scoring row
-        $p1 = 'INSERT INTO cmat_annual.scoring'
+
+        // Get form blowout that fits each group event registration entry
+        $q1 = 'SELECT r.registration_id, g.group_id, g.cmat_year, fb.form_blowout_id'
+            . ' FROM cmat_annual.registration r, cmat_annual."group" g, cmat_annual.group_member gm, cmat_annual.form_blowout fb'
+            . ' WHERE g.cmat_year = 15'
+            . ' AND r.scoring_id is null'
+            . ' AND r.is_paid = true'
+            . ' AND g.group_id = gm.group_id'
+            . ' AND r.competitor_id = gm.member_id'
+            . ' AND r.form_id = g.form_id'
+            . ' AND fb.cmat_year = g.cmat_year'
+            . ' AND fb.form_id = g.form_id'
+            . ' AND fb.gender_id = 3'
+            . ' AND fb.level_id = 0'
+            . ' AND fb.age_group_id = 0';
+
+        // Create a new individual event scoring row
+        $p2 = 'INSERT INTO cmat_annual.scoring'
             . ' (cmat_year, competitor_id, form_blowout_id)'
             . ' VALUES'
             . ' (%d, %d, %d)';
+
         // Link registration row to new scoring row
-        $p2 = 'UPDATE cmat_annual.registration'
+        $p3 = 'UPDATE cmat_annual.registration'
             . " SET scoring_id = currval('cmat_annual.scoring_scoring_id_seq')"
             . ' WHERE registration_id = %d';
+
+        // Create a new group event scoring row
+        $p4 = 'INSERT INTO cmat_annual.scoring'
+            . ' (cmat_year, group_id, form_blowout_id)'
+            . ' VALUES'
+            . ' (%d, %d, %d)';
+
 
 
         // Sorted results
         $individualScoringList = array();
+        $groupScoringList = array();
 
 
         // Database calls
@@ -41,6 +69,11 @@
         $r = Db::query($q0);
         while ($row = Db::fetch_array($r)) {
             $individualScoringList[] = $row;
+        }
+        Db::free_result($r);
+        $r = Db::query($q1);
+        while ($row = Db::fetch_array($r)) {
+            $groupScoringList[] = $row;
         }
         Db::free_result($r);
 
@@ -51,10 +84,16 @@
         Db::query('BEGIN');
         foreach ($individualScoringList as $k => $v) {
             // Create scoring row
-	    Db::query(sprintf($p1, $v['cmat_year'], $v['competitor_id'], $v['form_blowout_id']));
-            // Link registration row to scoring row
-            Db::query(sprintf($p2, $v['registration_id']));
+	    Db::query(sprintf($p2, $v['cmat_year'], $v['competitor_id'], $v['form_blowout_id']));
+            // Link registration row to new scoring row
+            Db::query(sprintf($p3, $v['registration_id']));
 	}
+        foreach ($groupScoringList as $k => $v) {
+            // Create scoring row
+            Db::query(sprintf($p4, $v['cmat_year'], $v['group_id'], $v['form_blowout_id']));
+            // Link registration row to new scoring row
+            Db::query(sprintf($p3, $v['registration_id']));
+        }
         Db::query('COMMIT');
         Db::close($conn);
     }
@@ -72,14 +111,19 @@
 <?php if ($doCreate) { ?>
     <p>
       Done!
-      <?php echo count($individualScoringList); ?> Individual scoring rows added.
-      <pre><?php print_r($individualScoringList);?></pre>
+      <ul>
+        <li><?php echo count($individualScoringList); ?> individual scoring rows added.</li>
+        <li><?php echo count($groupScoringList); ?> group scoring rows added.</li>
+      </ul>
     </p>
 <?php } else { ?>
     <p>
       There is one scoring entry for every entity (competitor or group) that gets scored in some event.
       For individual events, there is exactly one scoring entry per registration entry.
       For group events, there is exactly one scoring entry per group.
+    </p>
+    <p>
+      Only registration rows that are not already linked to a scoring row and have paid are populated.
     </p>
     <form action="" method="post">
       <input type="submit" name="create" value="Populate Scoring Table"/>
