f09633c159a7723a0f4eb33579800162694cb121
Rupert Chen <rupert.chen@gmail.com>
2007-03-11 01:26:14 +0000

    Scrutiny check for matching form and gender between the registration table and the scoring table.

--- www/setup/populate_scoring_table.php
+++ www/setup/populate_scoring_table.php
@@ -13,10 +13,11 @@
         // Get form blowout that fits each individual event registration entry
 	$q0 = 'SELECT r.registration_id, r.competitor_id, c.cmat_year, fb.form_blowout_id'
 	    . ' FROM cmat_annual.registration r, cmat_annual.competitor c, cmat_annual.form_blowout fb, cmat_enum.form f'
-	    . ' WHERE r.scoring_id is null'
+	    . ' WHERE r.scoring_id IS NULL'
             . ' AND r.is_paid = true'
             . ' AND c.cmat_year = 15'
             . ' AND f.is_group = false'
+            . ' AND f.ring_configuration_id <> 1'
             . ' AND r.competitor_id = c.competitor_id'
             . ' AND r.form_id = f.form_id'
             . ' AND fb.cmat_year = c.cmat_year'
@@ -29,7 +30,7 @@
         $q1 = 'SELECT r.registration_id, g.group_id, g.cmat_year, fb.form_blowout_id'
             . ' FROM cmat_annual.registration r, cmat_annual."group" g, cmat_annual.group_member gm, cmat_annual.form_blowout fb'
             . ' WHERE g.cmat_year = 15'
-            . ' AND r.scoring_id is null'
+            . ' AND r.scoring_id IS NULL'
             . ' AND r.is_paid = true'
             . ' AND g.group_id = gm.group_id'
             . ' AND r.competitor_id = gm.member_id'
@@ -40,20 +41,40 @@
             . ' AND fb.level_id = 0'
             . ' AND fb.age_group_id = 0';
 
+        // Get form blowout that fits each push hands registration entry
+        $q2 = 'SELECT r.registration_id, r.competitor_id, c.cmat_year, fb.form_blowout_id'
+            . ' FROM cmat_annual.registration r, cmat_annual.competitor c, cmat_annual.form_blowout fb, cmat_enum.form f'
+            . ' WHERE r.scoring_id IS NULL'
+            . ' AND r.is_paid = true'
+            . ' AND f.ring_configuration_id = 1'
+            . ' AND r.form_id = f.form_id'
+            . ' AND r.competitor_id = c.competitor_id'
+            . ' AND fb.cmat_year = c.cmat_year'
+            . ' AND fb.form_id = r.form_id'
+            . ' AND fb.gender_id = c.gender_id'
+            . ' AND fb.level_id = 0'
+            . ' AND fb.age_group_id = 0';
+
         // Create a new individual event scoring row
-        $p2 = 'INSERT INTO cmat_annual.scoring'
+        $p0 = 'INSERT INTO cmat_annual.scoring'
             . ' (cmat_year, competitor_id, form_blowout_id)'
             . ' VALUES'
             . ' (%d, %d, %d)';
 
         // Link registration row to new scoring row
-        $p3 = 'UPDATE cmat_annual.registration'
+        $p1 = 'UPDATE cmat_annual.registration'
             . " SET scoring_id = currval('cmat_annual.scoring_scoring_id_seq')"
             . ' WHERE registration_id = %d';
 
         // Create a new group event scoring row
-        $p4 = 'INSERT INTO cmat_annual.scoring'
+        $p2 = 'INSERT INTO cmat_annual.scoring'
             . ' (cmat_year, group_id, form_blowout_id)'
+            . ' VALUES'
+            . ' (%d, %d, %d)';
+
+        // Create a new push hands event scoring row
+        $p3 = 'INSERT INTO cmat_annual.scoring'
+            . ' (cmat_year, competitor_id, form_blowout_id)'
             . ' VALUES'
             . ' (%d, %d, %d)';
 
@@ -62,20 +83,28 @@
         // Sorted results
         $individualScoringList = array();
         $groupScoringList = array();
+        $pushHandsScoringList = array();
 
 
         // Database calls
         $conn = Db::connect();
+
         $r = Db::query($q0);
         while ($row = Db::fetch_array($r)) {
             $individualScoringList[] = $row;
         }
         Db::free_result($r);
+
         $r = Db::query($q1);
         while ($row = Db::fetch_array($r)) {
             $groupScoringList[] = $row;
         }
         Db::free_result($r);
+
+	$r = Db::query($q2);
+        while ($row = Db::fetch_array($r)) {
+            $pushHandsScoringList[] = $row;
+        }
 
 
         // Work
@@ -84,15 +113,21 @@
         Db::query('BEGIN');
         foreach ($individualScoringList as $k => $v) {
             // Create scoring row
-	    Db::query(sprintf($p2, $v['cmat_year'], $v['competitor_id'], $v['form_blowout_id']));
+	    Db::query(sprintf($p0, $v['cmat_year'], $v['competitor_id'], $v['form_blowout_id']));
             // Link registration row to new scoring row
-            Db::query(sprintf($p3, $v['registration_id']));
+            Db::query(sprintf($p1, $v['registration_id']));
 	}
         foreach ($groupScoringList as $k => $v) {
             // Create scoring row
-            Db::query(sprintf($p4, $v['cmat_year'], $v['group_id'], $v['form_blowout_id']));
+            Db::query(sprintf($p2, $v['cmat_year'], $v['group_id'], $v['form_blowout_id']));
             // Link registration row to new scoring row
-            Db::query(sprintf($p3, $v['registration_id']));
+            Db::query(sprintf($p1, $v['registration_id']));
+        }
+        foreach ($pushHandsScoringList as $k => $v) {
+            // Create scoring row
+            Db::query(sprintf($p3, $v['cmat_year'], $v['competitor_id'], $v['form_blowout_id']));
+            // Link registration row to new scoring row
+            Db::query(sprintf($p1, $v['registration_id']));
         }
         Db::query('COMMIT');
         Db::close($conn);
@@ -114,6 +149,7 @@
       <ul>
         <li><?php echo count($individualScoringList); ?> individual scoring rows added.</li>
         <li><?php echo count($groupScoringList); ?> group scoring rows added.</li>
+        <li><?php echo count($pushHandsScoringList); ?> push hands scoring rows added.</li>
       </ul>
     </p>
 <?php } else { ?>
