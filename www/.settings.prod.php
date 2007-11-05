fd3e5bd023d112f7777832565e1ed38638e4b0ee
Rupert Chen <rupert.chen@gmail.com>
2007-11-05 07:45:12 +0000

    Deleting some deprecated stuff from version 1.

--- www/.settings.prod.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $conf['CODE_PATH'] = '/var/cmat-o-matic/php';
-    $conf['WWW_PATH'] = 'http://1.1.1.1/';
-    $conf['DEBUG_MODE'] = false;
-    $conf['CMAT_YEAR'] = 15;
-?>
--- www/.settings.sandbox.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $conf['CODE_PATH'] = '/home/calwushu/dev_tera/cmat-o-matic/trunk/php';
-    $conf['WWW_PATH'] = 'http://192.168.1.105/~calwushu/cmat15/sandycom';
-    $conf['DEBUG_MODE'] = true;
-    $conf['CMAT_YEAR'] = 15;
-?>
