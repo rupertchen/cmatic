4c10e902a1aa06b13374a629b616eb478676d3e9
Rupert Chen <rupert.chen@gmail.com>
2007-12-11 08:53:17 +0000

    Cleaned up setup page.

--- clients/resources/js/setup.js
+++ clients/resources/js/setup.js
@@ -2,87 +2,115 @@ Ext.namespace('Setup');
 
 Setup.app = function () {
 
+    // ****************************************
     // private vars
+    var headerPanel;
+    var navTreePanel;
+    var mainPanel;
 
+
+    // ****************************************
     // private functions
 
+    /**
+     * Build the standard panels that will be used by the viewport
+     */
+    var buildPanels = function () {
+        headerPanel =  new Ext.Panel({
+            contentEl: 'header',
+            region: 'north'
+        });
+
+        navTreePanel = new Ext.tree.TreePanel({
+            region: 'west',
+            id: 'treePanel',
+            loader: new Ext.tree.TreeLoader({
+                preloadChildren: true,
+                clearOnLoad: false
+            }),
+            width: 150,
+            rootVisible: false,
+            root: new Ext.tree.AsyncTreeNode({
+                text: 'root',
+                children: [{
+                    id: '1',
+                    text: 'Event Parameters',
+                    children: [{
+                        id: '1.0',
+                        text: 'Division',
+                        leaf: true
+                    }, {
+                        id: '1.1',
+                        text: 'Sex',
+                        leaf: true
+                    }, {
+                        id: '1.2',
+                        text: 'Age Groups',
+                        leaf: true
+                    }]
+                },{
+                    id: '2',
+                    text: 'Event Management',
+                    children: [{
+                        id: '2.0',
+                        text: 'Available Events',
+                        leaf: true
+                    }, {
+                        id: '2.1',
+                        text: 'Event Schedule',
+                        leaf: true
+                    }]
+                }]
+            })
+        });
+
+        mainPanel = new Ext.TabPanel({
+            region: 'center',
+            id: 'mainPanel',
+            title: 'Foo',
+            defaults: {autoScroll: true},
+            activeItem: 0,
+            items: [{
+                xtype: 'panel',
+                title: 'FAQ',
+                contentEl: 'faq'
+            }]
+        });
+
+    };
+
+
+    // ****************************************
     // public space
     return {
         init: function () {
+            Ext.QuickTips.init();
+            this.buildViewport();
+            setTimeout(this.removeLoadingMask, 250);
+        },
 
-            var header =  new Ext.Panel({
-                contentEl: 'header',
-                region: 'north'
-            });
-
-            var tree = new Ext.tree.TreePanel({
-                region: 'west',
-                id: 'treePanel',
-                loader: new Ext.tree.TreeLoader({
-                    preloadChildren: true,
-                    clearOnLoad: false
-                }),
-                width: 150,
-                rootVisible: false,
-                root: new Ext.tree.AsyncTreeNode({
-                    text: 'root',
-                    children: [{
-                        id: '1',
-                        text: 'Event Parameters',
-                        children: [{
-                            id: '1.0',
-                            text: 'Division',
-                            leaf: true
-                        }, {
-                            id: '1.1',
-                            text: 'Sex',
-                            leaf: true
-                        }, {
-                            id: '1.2',
-                            text: 'Age Groups',
-                            leaf: true
-                        }]
-                    },{
-                        id: '2',
-                        text: 'Event Management',
-                        children: [{
-                            id: '2.0',
-                            text: 'Available Events',
-                            leaf: true
-                        }, {
-                            id: '2.1',
-                            text: 'Event Schedule',
-                            leaf: true
-                        }]
-                    }]
-                })
-            });
-
-            var main = new Ext.TabPanel({
-                region: 'center',
-                id: 'mainPanel',
-                title: 'Foo',
-                defaults: {autoScroll: true},
-                activeItem: 0,
-                items: [{
-                    xtype: 'panel',
-                    title: 'FAQ',
-                    contentEl: 'faq'
-                }]
-            });
-
-            // Everything
+        /**
+         * Build the Setup viewport
+         */
+        buildViewport: function () {
+            buildPanels();
             var viewport = new Ext.Viewport({
                 layout: 'border',
-                items: [header, tree, main]
+                items: [headerPanel, navTreePanel, mainPanel]
             });
 
-            // Removing "Loading"-related stuff when done
-            setTimeout(function () {
-                Ext.get('loading').remove();
-                Ext.get('loading-mask').fadeOut({duration: .25, remove: true});
-            }, 250);
+            return viewport;
+        },
 
+        /**
+         * Remove the loading mask
+         * TODO: Move this to some common area
+         */
+        removeLoadingMask: function () {
+            Ext.get('loading').remove();
+            Ext.get('loading-mask').fadeOut({duration: .25, remove: true});
         }
     };
 }();
+
+Ext.onReady(Setup.app.init, Setup.app);
